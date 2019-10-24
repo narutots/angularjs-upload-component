cff1aeacecd1ceabf189f741690fd582319b10a7
narutots <898698921@qq.com>
2019-10-24 15:32:29 +0800

    upload-component

--- app/uploadFileTest/uploadFileTest.js
+++ app/uploadFileTest/uploadFileTest.js
@@ -1,14 +1,16 @@
 'use strict'; 
 angular.module('uploadFileTest', [])
-.controller('uploadFileTestCtrl', [function() {
+.controller('uploadFileTestCtrl', function($scope) {
+      
+    var result=[];
+     $scope.uploadResult=[];
+    //回传数据方式1，方法回调 
+     $scope.uploadFileList=function(uploadResult)
+     {  result=uploadResult; 
+     }
+     // 回传数据方式2，数据绑定
+     result= $scope.uploadResult;
+ 
+   
 
-
-
-
-  
-
-
-
-
-
-}]);
+});
